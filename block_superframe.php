e276a04a17e531517392a75a5b37af6fbc916d74
jkfreij <jkfreij@gmail.com>
2019-07-16 14:06:10 +0200

    Modifying the view page

--- block_superframe.php
+++ block_superframe.php
@@ -82,7 +82,11 @@ class block_superframe extends block_base {
         $this->content->text .= '<br />' . get_string('message', 'block_superframe');
         //$this->content->text .= '<br /> <a href="' . $CFG->wwwroot . '/blocks/superframe/view.php">' .
             get_string('viewlink', 'block_superframe') . '</a>';
-        $url = new moodle_url('/blocks/superframe/view.php',['blockid'=>blockid]);
+
+
+
+            $url = new moodle_url('/blocks/superframe/view.php',['blockid'=>blockid]);
+
         $this->content->text.='<p>'. html_writer::link($url,get_string('viewlink', 'block_superframe')) . '</p>';
         return $this->content;
     }
