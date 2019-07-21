0e8ef5460d1880f8464eebd52646f9140e0eb37a
jkfreij <jkfreij@gmail.com>
2019-07-21 11:19:39 +0300

    Instance settings, capabilities and permissions

--- block_superframe.php
+++ block_superframe.php
@@ -79,6 +79,7 @@ class block_superframe extends block_base {
         $this->content->footer = '';
         $this->content->text = get_string('welcomeuser', 'block_superframe',
                 $USER);
+        /**
         $this->content->text .= '<br />' . get_string('message', 'block_superframe');
         //$this->content->text .= '<br /> <a href="' . $CFG->wwwroot . '/blocks/superframe/view.php">' .
             get_string('viewlink', 'block_superframe') . '</a>';
@@ -88,6 +89,19 @@ class block_superframe extends block_base {
             $url = new moodle_url('/blocks/superframe/view.php',['blockid'=>blockid]);
 
         $this->content->text.='<p>'. html_writer::link($url,get_string('viewlink', 'block_superframe')) . '</p>';
+         *
+         **/
+
+        // Check user permission to see the view page.
+        $usercontext = context_user::instance($USER->id);
+
+        if (has_capability('block/superframe:seeviewpage', $usercontext)) {
+            // Add the block id to the Moodle URL for the view page.
+            $blockid = $this->instance->id;
+            $url = new moodle_url('/blocks/superframe/view.php', ['blockid' => $blockid]);
+            $this->content->text .= '<p>' . html_writer::link($url,
+                    get_string('viewlink', 'block_superframe')) . '</p>';
+        }
         return $this->content;
     }
 
